ef7a5b85742685e418cb437ce0699cb56051569a
RAM111111 <r.r.99@windowslive.com>
2021-12-10 00:09:44 +0300

    Initial Commit

---
+++ North East South West North East South West North East South West/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  North East South West North East South West North East South West
+//
+//  Created by R on 06/05/1443 AH.
+//  Copyright © 1443 R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
